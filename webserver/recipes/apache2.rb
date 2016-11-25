250b2722127dd23223b6f23047d5cc52ebb0d2ac
Nguyen Duc Trung Dung <dung.nguyendt@gmail.com>
2016-11-25 16:29:59 +0700

    Add files via upload

---
+++ webserver/recipes/apache2.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: .
+# Recipe:: httpd
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
+package 'httpd' do
+	action :install
+end
+
+service 'httpd' do
+	action [:enable, :start]
+end
